40befe4cb6078c2cd08fe8361f4a43512ce73974
Hannah Jacoby <hjacoby@grubhub.com>
2019-10-26 10:22:27 -0600

    port adjustment

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require ("express")
 const axios = require ("axios")
 const cheerio = require ("cheerio")
 const mongoose = require ("mongoose")
-const PORT = 8080
+// const PORT = 8080
 const app = express()
+
+var port = process.env.PORT || 8080;
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -137,8 +139,11 @@ app.get("/", function(req, res) {
 });
 
 
-app.listen(
-    PORT, () => {
-        console.log("App is listening on 8080")
-    }
-)
+// app.listen(
+//     PORT, () => {
+//         console.log("App is listening on 8080")
+//     }
+// )
+
+var server=app.listen(port,function() {
+  console.log("app running on port 8080"); });
